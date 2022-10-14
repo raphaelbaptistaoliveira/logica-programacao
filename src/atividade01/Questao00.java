e4b27690ee3a30a3c9316c1b9649314d1be2f769
raphael baptista <raphaelbaptista3@gmail.com>
2022-10-14 19:50:03 -0300

    questao00

---
+++ src/atividade01/Questao00.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Questao00 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        // nome, idade, altura, sexo
+        System.out.println("Digite seu nome");
+        String nome = ler
+        System.out.println("Digite seu idade");
+        System.out.println("Digite seu altura");
+        System.out.println("Digite seu sexo");
+    }
+}
