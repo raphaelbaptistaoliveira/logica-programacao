5222925e91421a481a46192991b14190f3fcc9bf
raphael baptista <raphaelbaptista3@gmail.com>
2022-10-14 23:15:55 -0300

    atualização

--- src/atividade01/Questao01.java
+++ src/atividade01/Questao01.java
@@ -6,21 +6,22 @@ public class Questao01 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         float nota1,nota2,media;
-        System.out.println("Digite a primeira nota");
-        nota1 = ler.nextInt();
-        System.out.println("Digite a segunda nota");
-        nota2 = ler.nextInt();
+        System.out.println("Digite a primeira nota: ");
+        nota1 = ler.nextFloat();
+        System.out.println("Digite a segunda nota: ");
+        nota2 = ler.nextFloat();
 
         media = (nota1 + nota2) /2;
 
-        if (media > 7){
+        if (media >= 7){
             System.out.println("O aluno foi Aprovado");
-        } else if (media < 7){
+        }else{
             System.out.println("O aluno foi Reprovado");
         }
-
-        System.out.println("Primeira nota:"+nota1);
-        System.out.println("Segunda nota:"+nota2);
-        System.out.println("A média final do aluno e:"+media);
+            
+        ler.close();
+        System.out.println("Primeira nota: "+nota1);
+        System.out.println("Segunda nota: "+nota2);
+        System.out.println("A média final do aluno e: "+media);
     }
 }
