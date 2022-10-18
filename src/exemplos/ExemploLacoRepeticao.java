f996996da0ce415e39ccd99e5e5cd72c382f8d23
raphael baptista <raphaelbaptista3@gmail.com>
2022-10-18 19:48:23 -0300

    laco repeticao

---
+++ src/exemplos/ExemploLacoRepeticao.java
@@ -0,0 +1,13 @@
+package exemplos;
+
+public class ExemploLacoRepeticao {
+    public static void main(String[] args) {
+        int contar = 1; // i
+        while(contar <=10){
+            System.out.println(contar);
+            System.out.println(" < dentro do loop while");
+            // contar = contar + 1;
+            contar++;
+        }
+    }
+}
