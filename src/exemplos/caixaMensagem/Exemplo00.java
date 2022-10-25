8510318f96971e87ec5fdf5f2242dbad2d8821bb
raphael baptista <raphaelbaptista3@gmail.com>
2022-10-24 21:25:00 -0300

    caixa mensagem

--- src/exemplos/caixaMensagem/Exemplo00.java
+++ src/exemplos/caixaMensagem/Exemplo00.java
@@ -1,7 +1,11 @@
 package exemplos.caixaMensagem;
 
+import javax.swing.JOptionPane;
+
 public class Exemplo00 {
     public static void main(String[] args) {
-        
+        JOptionPane.showMessageDialog(null, "Java com JOptionPane");
+        String nome = JOptionPane.showInputDialog("Digite o seu nome");
+        JOptionPane.showMessageDialog(null, "Bem vindo " + nome);
     }
 }
