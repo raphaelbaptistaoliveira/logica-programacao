e2ca9e781723851d1da5c9121223b6ee1e4cee4c
raphael baptista <raphaelbaptista3@gmail.com>
2022-10-26 21:31:04 -0300

    atividade alterar 26_10

--- src/exemplos/exemplosArray/Exemplo03.java
+++ src/exemplos/exemplosArray/Exemplo03.java
@@ -5,20 +5,13 @@ import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class Exemplo03 {
-    // Desafio incluir a opção alterar!
     public static void main(String[] args) {
         // arraylista é uma classe java mais flexivel para manipular uma lista
         ArrayList<String> lista = new ArrayList<String>();
         int selecionado = 0;
-        Object[] opcoes = { "Incluir", "Listar", "Apagar", "Finalizar" };
+        Object[] opcoes = { "Incluir", "Listar", "Apagar", "Alterar" , "Finalizar" };
         do {
-            selecionado = JOptionPane.showOptionDialog(
-                    null,
-                    "O que você deseja?",
-                    "Exemplo",
-                    JOptionPane.DEFAULT_OPTION,
-                    JOptionPane.WARNING_MESSAGE,
-                    null, opcoes, opcoes[0]);
+            selecionado = JOptionPane.showOptionDialog(null,"O que você deseja?","Exemplo",JOptionPane.DEFAULT_OPTION,JOptionPane.WARNING_MESSAGE,null, opcoes, opcoes[0]);
             if (selecionado == 0) { // opcoes[selecionado].equals("Incluir")
                 String item = JOptionPane.showInputDialog("Digite");
                 lista.add(item); // guarda na lista o item digitado
@@ -35,14 +28,7 @@ public class Exemplo03 {
                 for (int i = 0; i < opcaoApagar.length; i++) {
                     opcaoApagar[i] = lista.get(i);
                 }
-                Object itemSelecionado = JOptionPane.showInputDialog(
-                        null,
-                        "Selecione o item para apagar",
-                        "Apagar",
-                        JOptionPane.INFORMATION_MESSAGE,
-                        null,
-                        opcaoApagar,
-                        opcaoApagar[0]);
+                Object itemSelecionado = JOptionPane.showInputDialog(null,"Selecione o item para apagar","Apagar",JOptionPane.INFORMATION_MESSAGE,null,opcaoApagar,opcaoApagar[0]);
                 // System.out.println(itemSelecionado.toString());
                 for (int i = 0; i < lista.size(); i++) {
                     if (itemSelecionado.toString().equals(lista.get(i))) {
@@ -50,6 +36,19 @@ public class Exemplo03 {
                     }
                 }
             }
-        } while (selecionado != 3);
+            if (selecionado == 3) {
+                Object[] opcaoAlterar = new Object[lista.size()];
+                for (int i = 0; i < opcaoAlterar.length; i++) {
+                    opcaoAlterar[i] = lista.get(i);
+                }
+                Object itemSelecionado = JOptionPane.showInputDialog(null,"Selecione o item para alteração","Alterar",JOptionPane.INFORMATION_MESSAGE,null,opcaoAlterar,opcaoAlterar[0]);
+                String item = JOptionPane.showInputDialog("Digite");
+                for (int i = 0; i < opcaoAlterar.length; i++) {
+                    if (itemSelecionado.equals(lista.get(i))) {
+                        lista.set(i, item);
+                    }
+            }   }
+
+        } while (selecionado != 4);
     }
 }
