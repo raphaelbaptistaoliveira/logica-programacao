c617f466c63cdc5fbbbf4c2b54f27503e47e6fd2
raphael baptista <raphaelbaptista3@gmail.com>
2022-10-27 21:48:45 -0300

    Exemplo04

---
+++ src/exemplos/exemplosArray/Exemplo04.java
@@ -0,0 +1,8 @@
+package exemplos.exemplosArray;
+
+public class Exemplo04 {
+    public static void main(String[] args) {
+        System.out.println("TESTE".equals("TESTE"));
+        System.out.println("TESTE".equals("teste"));
+    }
+}
